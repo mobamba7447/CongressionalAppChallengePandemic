abe343042536b222f1888b789bd018572782653c
Ryan Bascos <ryanjbascos@gmail.com>
2020-07-25 21:53:51 -0700

    big brain

--- ProjectPandemic/ProjectPandemic/ContentView.swift
+++ ProjectPandemic/ProjectPandemic/ContentView.swift
@@ -2,18 +2,42 @@
 //  ContentView.swift
 //  ProjectPandemic
 //
-//  Created by Jahan khan on 7/16/20.
+//  Created by Jahan Khan on 7/16/20.
 //  Copyright © 2020 Ryan Bascos Ashlyn Burrus Jahan Khan. All rights reserved.
 //
-
+ 
 import SwiftUI
-
+ 
 struct ContentView: View {
+    
+    @State var currentPage = pages.docs
+    
     var body: some View {
-        Text("Project Pandemic")
+        
+        ZStack {
+            
+            BarView(currentPage: $currentPage)
+            
+            VStack {
+                Text("Project Pandemic")
+                    .padding()
+                Text("Copyright © 2020 Ryan Bascos, Ashlyn Burrus Jahan Khan. All rights reserved.")
+                    .font(.caption)
+                if currentPage == .docs {
+                    Text("docs")
+                }
+                if currentPage == .convos {
+                    Text("convos")
+                }
+                if currentPage == .profile {
+                    Text("profile")
+                }
+            }
+            
+        }
     }
 }
-
+ 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
